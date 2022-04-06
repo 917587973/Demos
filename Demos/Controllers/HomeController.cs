b03263b0cf96ca6ca9d505499deef452f9fecb75
avinash <avinash.sanglikar@gmail.com>
2022-04-06 12:50:30 +0530

    Add project files.

---
+++ Demos/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Demos.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Demos.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public static List<Persons> personsslist = new List<Persons>();
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+           
+            return View(personsslist);
+        }
+
+        public ActionResult Results()
+        {
+            
+            return View(personsslist);
+        }
+
+        public ActionResult Delete(string? i)
+        {
+            var st = personsslist.Find(c => c.FirstName == i);
+            personsslist.Remove(st);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Persons ctycreate)
+        {
+           
+                personsslist.Add(ctycreate);
+                ViewBag.message = "The user " + ctycreate.FirstName + " is saved successfully";
+                return RedirectToAction("Index");
+            
+        }
+    }
+}
---
+++ Demos/Models/Persons.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Demos.Models
+{
+    public class Persons
+    {
+        public Persons()
+        {
+         
+        }
+
+        [Required(ErrorMessage = "Please Enter First Name..")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Last Name...")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Please Enter City...")]
+        [Display(Name = "City")]       
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please Enter State...")]
+        [Display(Name = "State")]
+        public string State { get; set; }
+
+        [Required(ErrorMessage = "Select the Gender...")]
+        [Display(Name = "Gender")]
+        public string Gender { get; set; }
+    }
+}
